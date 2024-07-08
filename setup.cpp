b0e3bd7506ffcb9fa056fd75b1760b119802c9a3
Punyapat Ratsamaisuk <166913067+kaopj@users.noreply.github.com>
2024-07-08 20:21:56 +0700

    Create setup.cpp

---
+++ setup.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+#define int long long
+#define lgm cin.tie(0)->sync_with_stdio(0);
+#define be(x) x.begin(),x.end()
+signed main() {
+  lgm;
+  int n;
+}
